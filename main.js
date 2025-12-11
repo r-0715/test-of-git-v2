a82473f3cecd5d1349dc94c9ebbd3b49ef63e19b
r-0715 <ritsukiwaritsu@gmail.com>
2025-12-11 22:21:21 +0900

    2nd-commit

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    //
+    //ul(親要素取得)
+    const list = document.getElementById('list');
+    //
+    //子要素作成
+    const li = document.createElement('li');
+    //
+    //
+    
+});
